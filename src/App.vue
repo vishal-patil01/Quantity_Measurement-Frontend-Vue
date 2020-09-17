f0db92b9a665e31c3473e60d568268e40a8e6734
vishal-patil01 <vishalpatil01@hotmail.com>
2020-09-17 14:58:18 +0530

    Initial commit : Quantity Measurement  Project Created.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>Welcome To Quantity Measurement</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: { },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: -webkit-fill-available;
+}
+</style>
